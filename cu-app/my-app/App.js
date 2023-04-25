d73d1f84731c58ecd6c37b4fa22e8cb630727480
MJ <tekaganaa@gmail.com>
2023-04-25 12:48:32 +0800

    cu-2

--- cu-app/my-app/App.js
+++ cu-app/my-app/App.js
@@ -1,10 +1,17 @@
-import { StatusBar,View  } from 'expo-status-bar';
-
-export default function App() {
-  return (
-    
-   <StatusBar style="auto" />
-     
+import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './source/Huudas/Home';
+const stack = createNativeStackNavigator();
+export default function App(){
+  return(
+    <NavigationContainer>
+      <stack.Navigator>
+        <stack.Screen
+        name='Home'
+        component={HomeScreen}
+        options={{headerShown:false}}/>
+      </stack.Navigator>
+    </NavigationContainer>
   );
-}
-
+};
--- cu-app/my-app/source/Huudas/Home.js
+++ cu-app/my-app/source/Huudas/Home.js
@@ -1,11 +1,14 @@
-import {StyleSheet, View } from 'react-native';
+import { StyleSheet, View, StatusBar, SafeAreaView, Text } from 'react-native';
+import { MaterialCommunityIcons, Ionicons,EvilIcons} from '@expo/vector-icons';
 export default function App() {
     return (
 
         <SafeAreaView style={styles.container}>
             <View style={styles.cuv0}>
                 <View style={styles.cuv1}>
-                <MaterialCommunityIcons name="list" style={styles.icon1} />
+                    <Ionicons style={styles.icon1} name="list" />
+                    <Text style={styles.tex}>297 </Text>
+                <EvilIcons style={{fontSize:40}} name='sc-pinterest'/>
                 </View>
                 <View style={styles.cuv2}></View>
                 <View style={styles.cuv3}></View>
@@ -19,12 +22,26 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "blue",
-        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0, 
+        backgroundColor: "white",
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     },
-    cuv1:{
+    icon1: {
+        fontSize: 40,
+        alignItems: "center",
+        flexDirection: 'row',
+        marginLeft: 10,
+    },
+    cuv1: {
+        flexDirection: 'row',
+        fontWeight: "bold",
+        // gap: 20,
+        alignItems: "center",
 
-    }
+    },
+    tex: {
+        fontSize: 23,
+        marginLeft:10,
+    },
 }
 )
 
