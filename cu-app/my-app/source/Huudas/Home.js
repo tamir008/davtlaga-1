c573d14f23d88f4da0c29b1fa2f9086784125fb8
MJ <112378583+tamir008@users.noreply.github.com>
2023-05-08 16:51:04 +0800

    3

--- cu-app/my-app/source/Huudas/Home.js
+++ cu-app/my-app/source/Huudas/Home.js
@@ -1,36 +1,43 @@
-import { StyleSheet, View, StatusBar, SafeAreaView, Text,ImageBackground } from 'react-native';
-import { MaterialCommunityIcons, Ionicons, EvilIcons, Entypo,Feather, } from '@expo/vector-icons';
+import { StyleSheet, View, StatusBar, SafeAreaView, Text, ImageBackground, Image } from 'react-native';
+import { MaterialCommunityIcons, Ionicons, EvilIcons, Entypo, Feather, } from '@expo/vector-icons';
 //https://cdn.zochil.shop/zochil.mn/cover.png
 export default function App() {
-    const image={uri:"https://cdn.zochil.shop/zochil.mn/cover.png"};
+    const image = { uri: "https://cdn.zochil.shop/zochil.mn/cover.png" };
     const localImage = require("../../assets/cu_logo.jpg");
+
+
+
     return (
 
         <SafeAreaView style={styles.container}>
-            <ImageBackground source={localImage}resizeMode='cover' style={styles.cont} >
-            <View style={styles.cuv0}>
-                <View style={styles.cuv1}>
-                    <Ionicons style={styles.icon1} name="list" />
-                    <Text style={styles.tex}>297 </Text>
-                    <EvilIcons style={{ fontSize: 35, marginBottom: 5 }} name='sc-pinterest' />
-                    <View style={styles.icon2}>
-                        <Ionicons style={{ fontSize: 30}} name="search" />       
+            <ImageBackground source={localImage} resizeMode='cover' style={styles.cont} >
+                <View style={styles.cuv0}>
+                    <View style={styles.cuv1}>
+                        <Ionicons style={styles.icon1} name="list" />
+                        <Text style={styles.tex}>297 </Text>
+                        <EvilIcons style={{ fontSize: 35, marginBottom: 5 }} name='sc-pinterest' />
+                        <View style={styles.icon2}>
+                            <Ionicons style={{ fontSize: 30 }} name="search" />
+                        </View>
+                        <Entypo style={{ fontSize: 30, marginLeft: 10 }} name="bell" />
                     </View>
-                    <Entypo style={{ fontSize: 30, marginLeft:10}} name="bell" />
+                    <View style={styles.cuv2}>
+                        <View style={styles.shat}>
+                            <Text style={{}}>Check-in урамшуулал</Text>
+                            <Text>1-р шат</Text>
+                        </View>
+                        <View style={styles.kk}>
+                            <View style={styles.ognoo}>
+                                <Text style={{}}>Кофе американо</Text>
+                                <Text>Дуусах огноо:2023-10-23</Text>
+                                <Image style={styles.asuult} source={require('./download.png')} />
+                                <Image style={{ width: 100, height: 100 }} source={require('../Hot-Drinks-Amerikano.jpg')} />
+                            </View>
+                        </View>
+                    </View>
+                    <View style={styles.cuv3}></View>
+                    <View style={styles.cuv4}></View>
                 </View>
-                <View style={styles.cuv2}>
-                    <View style={styles.shat}>
-                    <Text style={{}}>Check-in урамшуулал</Text>
-                    <Text>1-р шат</Text>
-                    </View>
-                    <View style={styles.ognoo}>
-                    <Text style={{}}>Кофе американо</Text>
-                    <Text>Дуусах огноо</Text>
-                    </View>
-                </View>
-                <View style={styles.cuv3}></View>
-                <View style={styles.cuv4}></View>
-            </View>
             </ImageBackground>
         </SafeAreaView>
 
@@ -42,7 +49,7 @@ const styles = StyleSheet.create({
         flex: 0.4,
         backgroundColor: "white",
         paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-        
+
     },
     icon1: {
         fontSize: 35,
@@ -64,9 +71,19 @@ const styles = StyleSheet.create({
     icon2: {
         marginLeft: 170,
     },
-    cont:{
-        width:'100%',
-        height:'100%',
+    cont: {
+        width: '100%',
+        height: '100%',
+    },
+    asuult: {
+        width: 35,
+        height: 35,
+        borderRadius: 50,
+    },
+    kk:{
+        flexDirection:'row',
+        justifyContent:'center',
+
     },
 
 }
