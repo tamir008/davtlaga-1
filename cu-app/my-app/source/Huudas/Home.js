f1a83790a283971c272f1c000a465d5c2640918c
MJ <112378583+tamir008@users.noreply.github.com>
2023-05-09 13:11:38 +0800

    4

--- cu-app/my-app/source/Huudas/Home.js
+++ cu-app/my-app/source/Huudas/Home.js
@@ -1,45 +1,51 @@
-import { StyleSheet, View, StatusBar, SafeAreaView, Text, ImageBackground, Image,ScrollView, localImage } from 'react-native';
+import { StyleSheet, View, StatusBar, SafeAreaView, Text, ImageBackground, Image, ScrollView, localImage } from 'react-native';
 import { MaterialCommunityIcons, Ionicons, EvilIcons, Entypo, Feather, } from '@expo/vector-icons';
 //https://cdn.zochil.shop/zochil.mn/cover.png
 export default function App() {
     const image = { uri: "https://cdn.zochil.shop/zochil.mn/cover.png" };
-{/* <View style={styles.cu}> */}
+    {/* <View style={styles.cu}> */ }
     const localImage = require("../../assets/cu_logo.jpg");
-// </View>
+    // </View>
     return (
 
         <SafeAreaView style={styles.container}>
             <ScrollView>
-            <ScrollView horizontal={true} style={styles.scroll}>
-            <ImageBackground source={localImage} resizeMode='cover' style={styles.cont} >
-                <View style={styles.cuv0}>
-                    <View style={styles.cuv1}>
-                        <Ionicons style={styles.icon1} name="list" />
-                        <Text style={styles.tex}>297 </Text>
-                        <EvilIcons style={{ fontSize: 35, marginBottom: 5 }} name='sc-pinterest' />
-                        <View style={styles.icon2}>
-                            <Ionicons style={{ fontSize: 30 }} name="search" />
-                        </View>
-                        <Entypo style={{ fontSize: 30, marginLeft: 10 }} name="bell" />
-                    </View>
-                   <View style={styles.cu2}>
-                   <Text style={styles.t1}>Check-in урамшуулал</Text>
-                   <Text style={styles.t1}>1-р шат</Text>
-                   </View>
- 
- {/* <View style={styles.kk}>
+                <ScrollView horizontal={true} style={styles.scroll}>
+                    <ImageBackground source={localImage} resizeMode='cover' style={styles.cont} >
+                        <View style={styles.cuv0}>
+                            <View style={styles.cuv1}>
+                                <Ionicons style={styles.icon1} name="list" />
+                                <Text style={styles.tex}>297 </Text>
+                                <EvilIcons style={{ fontSize: 35, marginBottom: 5 }} name='sc-pinterest' />
+                                <View style={styles.icon2}>
+                                    <Ionicons style={{ fontSize: 30 }} name="search" />
+                                </View>
+                                <Entypo style={{ fontSize: 30, marginLeft: 10 }} name="bell" />
+                            </View>
+                            <View>
+                            <View style={styles.cu2}>
+                                <Text style={styles.t1}>Check-in урамшуулал</Text>
+                                <Text style={styles.t2}>1-р шат</Text>                                                                         
+                            </View>
                             <View style={styles.ognoo}>
-                                <Text style={{}}>Кофе американо</Text>
+                            <Text style={{}}>Кофе американо</Text>
+                            <Text>Дуусах огноо:2023-10-23</Text>
+                            <Image style={styles.asuult} source={require('./download.png')} />
+                            </View>
+                            </View>
+                            {/* <View style={styles.kk}>
+                            <View style={styles.ognoo}>
+ s                                <Texttyle={{}}>Кофе американо</Text>
                                 <Text>Дуусах огноо:2023-10-23</Text>
                                 <Image style={styles.asuult} source={require('./download.png')} />
                                 <Image style={{ width: 100, height: 100 }} source={require('../Hot-Drinks-Amerikano.jpg')} />
                             </View> */}
                         </View>
-                    <View style={styles.cuv3}></View>
-                    <View style={styles.cuv4}></View>
-               
-            </ImageBackground>
-            </ScrollView>
+                        <View style={styles.cuv3}></View>
+                        <View style={styles.cuv4}></View>
+
+                    </ImageBackground>
+                </ScrollView>
             </ScrollView>
         </SafeAreaView>
 
@@ -66,7 +72,6 @@ const styles = StyleSheet.create({
         alignItems: "center",
 
     },
-   
     tex: {
         fontSize: 21,
         marginLeft: 10,
@@ -83,10 +88,19 @@ const styles = StyleSheet.create({
         height: 35,
         borderRadius: 50,
     },
-  
+    cu2: {
+        flexDirection: "row",
+        justifyContent: 'space-between',
+        marginLeft:20,
+        alignItems: 'center',   
     },
+    ognoo:{
+        fontSize: 30,
+        marginLeft: 20,
+    },
+},
 
-    )
+)
 
 
 
